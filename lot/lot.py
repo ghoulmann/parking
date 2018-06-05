4564f2a7f4576cb21a3ca0daf43d8cb66e53b7aa
Rik Goldman <rikgoldman@gmail.com>
2018-06-05 06:21:39 -0400

    turn into a class

--- lot/lot.py
+++ lot/lot.py
@@ -2,32 +2,34 @@
 
 
 
-global CAPACITY
-CAPACITY = 75
+class Lot():
+
+    def __init__:
+        CAPACITY = 75
 
 
-def lot_details(used, *season, **ay):
-    capacity = CAPACITY
-    remaining = CAPACITY - used
-    lot = {'capacity': capacity, 'assignments': assigned,'available_spots': remaining}
-    return lot
+    def lot_details(used, *season, **ay):
+        capacity = CAPACITY
+        remaining = CAPACITY - used
+        lot = {'capacity': capacity, 'assignments': assigned,'available_spots': remaining}
+        return lot
 
-def donut(data, markers, title):
-    """returns div"""
-    import plotly
-    import plotly.graph_objs as go
-    from plotly.offline import plot
+    def donut(data, markers, title):
+        """returns div"""
+        import plotly
+        import plotly.graph_objs as go
+        from plotly.offline import plot
 
-    fig = {
-        'data': [
-        {'values': data,
-         'labels': markers, 'hole': .4,
-            'hoverinfo': "label+percent+name",
-            'type': 'pie'
-            }],
-        'layout': {
-            "title":title
+        fig = {
+            'data': [
+            {'values': data,
+             'labels': markers, 'hole': .4,
+                'hoverinfo': "label+percent+name",
+                'type': 'pie'
+                }],
+            'layout': {
+                "title":title
+                }
             }
-        }
 
-    return plot(fig, include_plotlyjs=True, output_type='div')
+        return plot(fig, include_plotlyjs=True, output_type='div')
